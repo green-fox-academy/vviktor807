f9735f051e8f5c6ded4b4c166397be56b269ace9
vviktor807 <viktor.varga807@gmail.com>
2019-04-03 12:59:05 +0200

    number operations

---
+++ week-01/day-3/twonumbers/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << 13 + 22 << std::endl;
+    std::cout << 13 - 22 << std::endl;
+    std::cout << 22 * 13 << std::endl;
+    std::cout << 22 / 13 << std::endl;
+    std::cout << 22 / 13. << std::endl;
+    std::cout << 22 % 13 << std::endl;
+    return 0;
+}
