9e3ebb3f8eb641d95febe5bf909cf1986636a2bb
vviktor807 <viktor.varga807@gmail.com>
2019-04-03 14:09:31 +0200

    some basic information aobut me

---
+++ week-01/day-3/basicinfo/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::string myname = "Varga Viktor";
+    std::cout << myname <<std::endl;
+    int myage = 24;
+    std::cout << myage <<std::endl;
+    double myheight = 1.85;
+    std::cout << myheight <<std::endl;
+    bool ImMarried = false;
+    std::cout << ImMarried <<std::endl;
+    return 0;
+}
