2d27533428fb2dfc861664394709d172d6e9f358
vviktor807 <viktor.varga807@gmail.com>
2019-04-03 11:55:16 +0200

    hello world but instead of world added 3 classmates name

---
+++ week-01/day-3/helloothers/helloothers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Anna!" << std::endl;
+    std::cout << "Hello, Sanyi!" << std::endl;
+    std::cout << "Hello, Alex!" << std::endl;
+    
+    return 0;
+}
