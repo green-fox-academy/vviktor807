22a939137160b01c53daacf626d76b28bd3cf513
vviktor807 <viktor.varga807@gmail.com>
2019-04-03 16:02:29 +0200

    computed the surface area and the volume of a cuboid

---
+++ week-01/day-3/cuboid/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int length = 10;
+    int width = 4;
+    int height = 5;
+    int SurfaceArea = (2 * 4 * 10) + (2 * 10 * 5) + (2 * 4 * 5);
+    std::cout << "Surface Area:" << SurfaceArea<< std::endl;
+    int Volume = 10 * 4 * 5;
+    std::cout <<"Volume:" << Volume << std::endl;
+    return 0;
+}
