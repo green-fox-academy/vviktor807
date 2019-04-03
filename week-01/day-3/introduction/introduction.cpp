b56bb5741ccc5d897ff0d3b546d5f035d0002ce3
vviktor807 <viktor.varga807@gmail.com>
2019-04-03 12:16:03 +0200

    made an introduction that contains my name, age and height

---
+++ week-01/day-3/introduction/introduction.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "My name is Viktor Varga!" << std::endl;
+    std::cout << 24 << std::endl;
+    std::cout << 1.85 << std::endl;
+    
+    return 0;
+}
