1364227629d5625ef504672d8161dee2ec4d4d91
vviktor807 <viktor.varga807@gmail.com>
2019-04-04 14:35:53 +0200

    iwontcheat

---
+++ week-01/day-3/iwontcheat/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int cheat = 1;
+    while(cheat <= 100){
+        std::cout << "I won't cheat on the exame!" <<std::endl;
+        cheat = cheat + 1;
+    }
+    return 0;
+}
