bdefe48d47baf64046b5c7aeb96e8b557ff23040
vviktor807 <viktor.varga807@gmail.com>
2019-04-05 13:32:10 +0200

    program that can tell if your number is even or odd

---
+++ week-01/day-3/evenodd/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int b;
+    std::cout << "Please enter a number:" <<std::endl;
+    std::cin >> b;
+
+    if (b % 2 == 0) {
+        std::cout << "Your number is even. " <<std::endl;
+    }else {
+        std::cout << "Your number is odd." <<std::endl;
+    }
+    return 0;
+}
