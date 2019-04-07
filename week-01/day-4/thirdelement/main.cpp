86a3d9303b415c0aae9a99a9b52a2ba293054872
vviktor807 <viktor.varga807@gmail.com>
2019-04-07 10:17:07 +0200

    printing out an element

---
+++ week-01/day-4/thirdelement/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// - Create an array variable named `q`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `q`
+int main() {
+
+    int q[4] = {4, 5, 6, 7};
+    std::cout << q[2] << std::endl;
+    return 0;
+}
