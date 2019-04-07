58881d4ff09ecaa3c27bd9fe0b8942146023416e
vviktor807 <viktor.varga807@gmail.com>
2019-04-07 10:49:38 +0200

    changeelement

---
+++ week-01/day-4/changeelements/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// - Create an array variable named `s`
+//   with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the 8 to 4
+// - Print the fourth element
+int main() {
+
+    int s[] = {1, 2, 3, 8, 5, 6};
+    s[3] = 4;
+    std::cout << s[3] << std::endl;
+    return 0;
+}
