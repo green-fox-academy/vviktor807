0a1fc335e1ddc3fe0fdf6626b27c2e30458044da
vviktor807 <viktor.varga807@gmail.com>
2019-04-07 10:45:18 +0200

    sumelements

---
+++ week-01/day-4/sumelements/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// - Create an array variable named `r`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+int main() {
+
+    int r[] = {54, 23, 66, 12};
+    std::cout << r[1] + r[2] << std::endl;
+    return 0;
+}
