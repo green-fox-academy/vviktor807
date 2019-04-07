1c339693c560d36fc6a7ecf3472bd3e5bde66e70
vviktor807 <viktor.varga807@gmail.com>
2019-04-07 11:08:45 +0200

    printelement

---
+++ week-01/day-4/printelement/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// - Create an array variable named `numbers`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print all the elements of `numbers`
+int main() {
+
+    int numbers[] = {4, 5, 6, 7};
+
+    std::cout << numbers[0] << std::endl;
+    std::cout << numbers[1] << std::endl;
+    std::cout << numbers[2] << std::endl;
+    std::cout << numbers[3] << std::endl;
+    return 0;
+}
