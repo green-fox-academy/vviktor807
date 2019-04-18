7f12594767936df0e4857275aec896972a72d3d2
vviktor807 <viktor.varga807@gmail.com>
2019-04-18 15:30:49 +0200

    feat(week-04/day-04): Pokemon

---
+++ week-03/day-04/pokemon/Pokemon.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Viktor on 2019. 04. 18..
+//
+
+#include "pokemon.h"
+
+Pokemon::Pokemon(const std::string& name, const std::string& type, const std::string& effectiveAgainst)
+{
+    _name = name;
+    _type = type;
+    _effectiveAgainst = effectiveAgainst;
+}
+
+bool Pokemon::isEffectiveAgainst(Pokemon anotherPokemon)
+{
+    return _effectiveAgainst == anotherPokemon._type;
+}
