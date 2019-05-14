09d815f5b0a9e2bbdb094057d8c717c0e7507307
vviktor807 <viktor.varga807@gmail.com>
2019-05-14 13:58:29 +0200

    feat(two_numbers): finished the exercise

---
+++ week-06/day-02/two_numbers/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 13;
+    int b = 22;
+    int c = a + b;
+    int d = a - b;
+    int e = b * a;
+    int f = b / a;
+    int g = b % a;
+    printf("%d \n", c);
+    printf("%d \n", d);
+    printf("%d \n", e);
+    printf("%d \n", f);
+    printf("%d \n", g);
+
+    return 0;
+}
