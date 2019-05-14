e1ae894c2f14b4f868411df133d49b1db6476402
vviktor807 <viktor.varga807@gmail.com>
2019-05-14 13:49:16 +0200

    feat(introduce_yourself): finished the exercise

---
+++ week-06/day-02/introduce_yourself/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    char name[20] = "Varga Viktor";
+    int age = 24;
+    float height = 1.86;
+
+    printf("%s \n", name);
+    printf("%d \n", age);
+    printf("%.2f \n", height);
+
+
+    return 0;
+}
