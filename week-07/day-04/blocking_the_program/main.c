f0bad561328fc6fab649c05e3297e9704b2eb025
vviktor807 <viktor.varga807@gmail.com>
2019-05-23 16:46:02 +0200

    feat(blocking_the_program): completed the exercise with interrupt

---
+++ week-07/day-04/blocking_the_program/main.c
@@ -0,0 +1,73 @@
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  Ac6
+ * @version V1.0
+ * @date    01-December-2013
+ * @brief   Default main function.
+ ******************************************************************************
+ */
+
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+
+void init_led_pin()
+{
+	__HAL_RCC_GPIOI_CLK_ENABLE();
+
+	GPIO_InitTypeDef gpio_config;
+
+	gpio_config.Pin = GPIO_PIN_1;
+	gpio_config.Mode = GPIO_MODE_OUTPUT_PP;
+	gpio_config.Pull = GPIO_NOPULL;
+	gpio_config.Speed = GPIO_SPEED_FAST;
+
+	HAL_GPIO_Init(GPIOI, &gpio_config);
+}
+
+void init_button_pin()
+{
+	__HAL_RCC_GPIOI_CLK_ENABLE();
+
+	GPIO_InitTypeDef gpio_config;
+
+	gpio_config.Pin = GPIO_PIN_11;
+	gpio_config.Mode = GPIO_MODE_IT_RISING;
+	gpio_config.Pull = GPIO_NOPULL;
+	gpio_config.Speed = GPIO_SPEED_FAST;
+
+	HAL_GPIO_Init(GPIOI, &gpio_config);
+
+	HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+	HAL_NVIC_SetPriority(EXTI15_10_IRQn, 16, 0);
+
+}
+
+uint32_t lock = 0;
+
+int main(void)
+{
+	HAL_Init();
+	init_led_pin();
+	init_button_pin();
+
+	while (1) {
+		if (lock) {
+			HAL_GPIO_TogglePin(GPIOI, GPIO_PIN_1);
+			HAL_Delay(250);
+		}
+
+	}
+}
+
+void EXTI15_10_IRQHandler()
+{
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_11);
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	lock++;
+}
+
+
